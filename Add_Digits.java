4d967b694410ccf666442e3995284a37d2b4a0af
21MH5A0375 <113345807+21MH5A0375@users.noreply.github.com>
2022-10-29 20:29:00 +0530

    import java.util.Scanner; class Main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,r,sum=0;         n=sc.nextInt();         while(true)         {            sum=0;         while(n>0)         {
                r=n%10;
                sum=sum+r;
                n=n/10;
            }
            if(sum<=9)
            break;
            else
            n=sum;
            }
            
            System.out.println(sum);
            
        }
    }

--- Add_Digits.java
+++ Add_Digits.java
@@ -1,27 +1,28 @@
 import java.util.Scanner;
-class main
+class Main
 {
-    public static void main (String args[])
+    public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        int r,sum=0,n;
+        int n,r,sum=0;
         n=sc.nextInt();
         while(true)
         {
-            sum=0;
-            while(n>0)
-            {
-                r=n%10;
-                sum=sum+r;
-                n=n/10;
-            }
-            if(sum<=9)
-            {
-                break;
-            }
-            else
-            n=sum;
+           sum=0;
+        while(n>0)
+        {
+
+            r=n%10;
+            sum=sum+r;
+            n=n/10;
         }
+        if(sum<=9)
+        break;
+        else
+        n=sum;
+        }
+        
         System.out.println(sum);
+        
     }
 }
