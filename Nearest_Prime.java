70f6928f413ca8a0b0163b5aa4236cacb84dcc32
21MH5A0375 <113345807+21MH5A0375@users.noreply.github.com>
2022-11-08 14:31:36 +0530

    import java.util.Scanner; class Main {     public static boolean prime(int n)     {         if(n==1)         return false;         for(int i=2;i<=(int)Math.sqrt(n);i++)         {             if(n%i==0)             return false;         }         return true;     }
        public static void main(String args[])
        {
            Scanner sc=new Scanner(System.in);
            int t=sc.nextInt();
            int d1,d2,i;
            while(t>0)
            {
              int n=sc.nextInt();  
            for(i=n;;i++)
            {
            if(prime(i))
            {
            d1=i;
            break;
            }
            }
            for(i=n;;i--)
            {
            if(prime(i))
            {
            d2=i;
            break;
            }
            }
            
            if((d1-n)>(n-d2))
            System.out.println(d2);
            else if((d1-n)==(n-d2))
            System.out.println(d2);
            else
            System.out.println(d1);
            t--;
            }
            
        }
    }

---
+++ Nearest_Prime.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+class Main
+{
+    public static boolean prime(int n)
+    {
+        if(n==1)
+        return false;
+        for(int i=2;i<=(int)Math.sqrt(n);i++)
+        {
+            if(n%i==0)
+            return false;
+        }
+        return true;
+    }
+    
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        int d1,d2,i;
+        while(t>0)
+        {
+          int n=sc.nextInt();  
+        for(i=n;;i++)
+        {
+        if(prime(i))
+        {
+        d1=i;
+        break;
+        }
+        }
+        for(i=n;;i--)
+        {
+        if(prime(i))
+        {
+        d2=i;
+        break;
+        }
+        }
+        
+        if((d1-n)>(n-d2))
+        System.out.println(d2);
+        else if((d1-n)==(n-d2))
+        System.out.println(d2);
+        else
+        System.out.println(d1);
+        t--;
+        }
+        
+    }
+}
