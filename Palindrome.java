e2a44bbcebd877b92585a9d25c1e7d8ce663fdd1
21MH5A0375 <113345807+21MH5A0375@users.noreply.github.com>
2022-11-08 14:30:03 +0530

    import java.util.Scanner; class Mainn {     public static boolean palin(int n)     {         int r,temp=n,sum=0;         while(n>0)         {             r=n%10;             sum=sum*10+r;             n=n/10;         }         if(temp==sum)         return true;         else         return false;     }     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         if(palin(n))         System.out.println("True");         else         System.out.println("False");     }
    }

---
+++ Palindrome.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class Mainn
+{
+    public static boolean palin(int n)
+    {
+        int r,temp=n,sum=0;
+        while(n>0)
+        {
+            r=n%10;
+            sum=sum*10+r;
+            n=n/10;
+        }
+        if(temp==sum)
+        return true;
+        else
+        return false;
+    }
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(palin(n))
+        System.out.println("True");
+        else
+        System.out.println("False");
+    }
+    
+}
