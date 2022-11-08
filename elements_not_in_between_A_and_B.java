12064904dca71ddd907ebed26db02ed40c358105
21MH5A0375 <113345807+21MH5A0375@users.noreply.github.com>
2022-11-08 15:50:28 +0530

    import java.util.Scanner; class Main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int x[],n=sc.nextInt(),count=0;         x=new int[n];         for(int i=0;i<n;i++)         x[i]=sc.nextInt();         int a=sc.nextInt();         int b=sc.nextInt();         for(int i=0;i<n;i++)         {             if(x[i]<a || x[i]>b)             {                 System.out.print(x[i]+" ");                 count=1;             }         }         if(count==0)         System.out.print("-1");
        }
    }

--- elements_not_in_between_A_and_B.java
+++ elements_not_in_between_A_and_B.java
@@ -1,28 +1,25 @@
 import java.util.Scanner;
-class Sample
+class Main
 {
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        int n,x[],i,a,b,count=0;
-        n=sc.nextInt();
+        int x[],n=sc.nextInt(),count=0;
         x=new int[n];
-        for(i=0;i<n;i++)
+        for(int i=0;i<n;i++)
         x[i]=sc.nextInt();
-        a=sc.nextInt();
-        b=sc.nextInt();
-        for(i=0;i<n;i++)
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        for(int i=0;i<n;i++)
         {
-            
             if(x[i]<a || x[i]>b)
             {
                 System.out.print(x[i]+" ");
-                count++;
+                count=1;
             }
-           
-        }
-            if(count==0)
-            System.out.println("-1");
-    
+        } 
+        if(count==0)
+        System.out.print("-1");
+   
     }
 }
