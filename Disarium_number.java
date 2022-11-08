4792ce906bd9be18f2cadcfa0587fedb8017d021
21MH5A0375 <113345807+21MH5A0375@users.noreply.github.com>
2022-11-08 14:25:57 +0530

    import java.util.Scanner; class Main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt(),d,r,sum=0;         int temp=n;         d=(int)Math.log10(n)+1;         while(n>0)         {             r=n%10;             sum=sum+(int)Math.pow(r,d);             d--;             n=n/10;
            }
            if(sum==temp)
            System.out.println("True");
            else
            System.out.println("False");
            
        }
    }

--- Disarium_number.java
+++ Disarium_number.java
@@ -1,34 +1,24 @@
 import java.util.Scanner;
-class DisariumDemo
+class Main
 {
-   public static boolean isDisarium(int n)
-   {
-         int d=(int)Math.log10(n)+1;
-		 
-		 int sum=0,r,temp=n;
-		 while(n>0)
-		 {
-		   r=n%10;
-		   sum=sum+(int)Math.pow(r,d);
-		   n=n/10;
-		   d--;
-		 }
-		
-		 if(sum==temp)
-		    return true;
-		else 
-		   return false;	 	     	
-   }
-   public static void main(String args[])
-   {
-     Scanner sc=new Scanner(System.in);
-	 int n;
-	 n=sc.nextInt();
-	 
-	   if(isDisarium(n))
-	       System.out.println("True");
-	   else
-	       System.out.println("False");
-   }
-
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt(),d,r,sum=0;
+        int temp=n;
+        d=(int)Math.log10(n)+1;
+        while(n>0)
+        {
+            r=n%10;
+            sum=sum+(int)Math.pow(r,d);
+            d--;
+            n=n/10;
+            
+        }
+        if(sum==temp)
+        System.out.println("True");
+        else
+        System.out.println("False");
+        
+    }
 }
