4794962f4d106467456d126d4e9a650f18c72bb7
21MH5A0375 <113345807+21MH5A0375@users.noreply.github.com>
2022-11-08 14:24:19 +0530

    import java.util.Scanner; class Main {     public static int rev(int n)     {         int r,sum=0;         while(n>0)         {             r=n%10;             sum=sum*10+r;             n=n/10;         }         return sum;     }     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,t;         n=sc.nextInt();         t=rev(n);         if(n*n==rev(t*t))         System.out.println("True");         else         System.out.println("False");     }
    }

--- Adam_number.java
+++ Adam_number.java
@@ -1,30 +1,28 @@
 import java.util.Scanner;
 class Main
 {
-  public static void main(String args[])
-  {
+    public static int rev(int n)
+    {
+        int r,sum=0;
+        while(n>0)
+        {
+            r=n%10;
+            sum=sum*10+r;
+            n=n/10;
+        }
+        return sum;
+    }
+    public static void main(String args[])
+    {
         Scanner sc=new Scanner(System.in);
-        int n;
-         n=sc.nextInt();
-         int temp=n;
-         int semp=n*n;
-         int r,sum=0,add=0;
-         while(n>0)
-         {
-             r=n%10;
-             sum=sum*10+r;
-             n=n/10;
-         }
-         int d=sum*sum;
-         while(d>0)
-         {
-             r=d%10;
-             add=add*10+r;
-             d=d/10;
-         }
-         if(semp==add)
-            System.out.println("True");
+        int n,t;
+        n=sc.nextInt();
+        t=rev(n);
+        if(n*n==rev(t*t))
+        System.out.println("True");
         else
-            System.out.println("False");
-  }
+        System.out.println("False");
+    }
+    
+    
 }
