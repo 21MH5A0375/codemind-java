1a9106323d7199dde359c0f054e0ccc1c989b76f
21MH5A0375 <113345807+21MH5A0375@users.noreply.github.com>
2022-11-08 15:49:03 +0530

    import java.util.Scanner; class Main {     public static boolean odd(int n)     {         if(n%2==1)         return true;         else         return false;     }     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int x[],n=sc.nextInt(),sum=0,add=0;         x=new int[n];         for(int i=0;i<n;i++)         x[i]=sc.nextInt();         int a=sc.nextInt();         int b=sc.nextInt();         for(int i=0;i<n;i++)         {             if(x[i]<a)             sum=sum+x[i];         }         for(int i=0;i<n;i++)         {             if(x[i]>b)             add=add+x[i];         }
            System.out.println(add+sum);
        }
    }

--- sum_of_elements_not_between_A_and_B.java
+++ sum_of_elements_not_between_A_and_B.java
@@ -1,24 +1,33 @@
 import java.util.Scanner;
-class Sample
+class Main
 {
+    public static boolean odd(int n)
+    {
+        if(n%2==1)
+        return true;
+        else 
+        return false;
+    }
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        int n,x[],i,a,b,count=0,sum=0;
-        n=sc.nextInt();
+        int x[],n=sc.nextInt(),sum=0,add=0;
         x=new int[n];
-        for(i=0;i<n;i++)
+        for(int i=0;i<n;i++)
         x[i]=sc.nextInt();
-        a=sc.nextInt();
-        b=sc.nextInt();
-        for(i=0;i<n;i++)
-        {
-            count=count+x[i];
-            if(x[i]>=a && x[i]<=b)
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        for(int i=0;i<n;i++)
+        { 
+            if(x[i]<a)
             sum=sum+x[i];
+        } 
+        for(int i=0;i<n;i++)
+        {
+            if(x[i]>b)
+            add=add+x[i];
         }
-    
-            System.out.println(count-sum);
-    
+      
+        System.out.println(add+sum);
     }
 }
