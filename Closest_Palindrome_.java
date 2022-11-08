2040d572a89a60ec1df22fb44e2ae3776c85d64a
21MH5A0375 <113345807+21MH5A0375@users.noreply.github.com>
2022-11-08 14:28:54 +0530

    import java.util.Scanner; class Mainn {     public static boolean palin(int n)     {         int r,temp=n,sum=0;         while(n>0)         {             r=n%10;             sum=sum*10+r;             n=n/10;         }         if(temp==sum)         return true;         else         return false;     }     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int d1,d2;         for(int i=n+1;;i++)         {         if(palin(i))         {         d1=i;         break;         }         }         for(int i=n-1;;i--)         {         if(palin(i))         {         d2=i;         break;         }         }         if((d1-n)==(n-d2))         System.out.println(d2+" "+d1);         else if((d1-n)<(n-d2))         System.out.println(d1);         else         System.out.println(d2);     }
    }

--- Closest_Palindrome_.java
+++ Closest_Palindrome_.java
@@ -1,48 +1,47 @@
 import java.util.Scanner;
-class Palindrome
+class Mainn
 {
-   public static boolean isPalindrome(int n)
-   {
-         int temp=n,r,sum=0;
-		 while(n>0)
-		 {
-		     r=n%10;
-			 sum=sum*10+r;
-			 n=n/10;
-		 }
-        if(sum==temp)
-           return true;
+    public static boolean palin(int n)
+    {
+        int r,temp=n,sum=0;
+        while(n>0)
+        {
+            r=n%10;
+            sum=sum*10+r;
+            n=n/10;
+        }
+        if(temp==sum)
+        return true;
         else
-           return false;
-
+        return false;
     }
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        int n,d1,d2;
-        n=sc.nextInt();
+        int n=sc.nextInt();
+        int d1,d2;
         for(int i=n+1;;i++)
         {
-            if(isPalindrome(i))
-            {
-                d1=i;
-                break;
-            }
-            
+        if(palin(i))
+        {
+        d1=i;
+        break;
+        }
         }
         for(int i=n-1;;i--)
         {
-            if(isPalindrome(i))
-            {
-                d2=i;
-                break;
-            }
+        if(palin(i))
+        {
+        d2=i;
+        break;
         }
-        if((d1-n)<(n-d2))
-            System.out.println(d1);
-        else if((d1-n)==(n-d2))
-            System.out.println(d2+" "+d1);
+        }
+        if((d1-n)==(n-d2))
+        System.out.println(d2+" "+d1);
+        else if((d1-n)<(n-d2))
+        System.out.println(d1);
         else
-            System.out.println(d2);
+        System.out.println(d2);
     }
+    
 }
